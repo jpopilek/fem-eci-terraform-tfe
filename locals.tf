53ad79e299b0234de850d1c00c5763b436978a9d
John Popilek <john.popilek@gmail.com>
2024-06-05 14:12:25 -0500

    set up github workspace

--- locals.tf
+++ locals.tf
@@ -9,7 +9,13 @@ locals {
       description         = "Example workspace description"
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "jpopilek/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+    "fem-eci-github" = {
+      description         = "Example automation workspace for github"
+      execution_mode      = "local" #set first time to local
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
